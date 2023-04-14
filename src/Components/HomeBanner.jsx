aad0d86ebd59a18cd86b4507fd8a929a7f6552a0
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-04-14 22:58:36 +0200

    add

--- src/Components/HomeBanner.jsx
+++ src/Components/HomeBanner.jsx
@@ -5,18 +5,19 @@ import bannerImageElement from '../assets/BannerElement.png'
 const HomeBanner = () => {
     return (
         <div className='mt-8 flex items-center'>
-            <div className='text-white flex flex-col items-start'>
+            <div className='text-white flex flex-col items-start mt-16'>
                 <p className='header'>Taste the authentic </p>
                 <p className='header'>Turkish cuisine</p>
-                <p>Among the best Saudi chefs in the world,<br/>serving you something beyond flavor. </p>
+                <p>Among the best Saudi chefs in the world,<br />serving you something beyond flavor. </p>
                 <button className='w-40 h-16 border-[#CC9D2F] border-8 text-[#CC9D2F] text-2xl'>Our Menu</button>
             </div>
 
-            <div className='flex'>
-                <img src={bannerImage} alt=''/>
-                <img className='w-11 h-14' src={bannerImageElement} alt=''/>
+            <div className='flex items-start absolute'>
+                <img src={bannerImage} alt='' />
+                <img className='element' src={bannerImageElement} alt='' />
             </div>
-            
+
+
         </div>
     );
 };

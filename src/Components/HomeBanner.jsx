f0a474526bed7f527d472face05920c02b69cf87
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-04-14 17:05:01 +0200

    add

--- src/Components/HomeBanner.jsx
+++ src/Components/HomeBanner.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import bannerImage from '../assets/HomeBanner.png'
+import bannerImageElement from '../assets/BannerElement.png'
 
 const HomeBanner = () => {
     return (
@@ -7,12 +8,13 @@ const HomeBanner = () => {
             <div className='text-white flex flex-col items-start'>
                 <p className='header'>Taste the authentic </p>
                 <p className='header'>Turkish cuisine</p>
-                <p>Among the best Saudi chefs in the world, serving you something beyond flavor. </p>
+                <p>Among the best Saudi chefs in the world,<br/>serving you something beyond flavor. </p>
                 <button className='w-40 h-16 border-[#CC9D2F] border-8 text-[#CC9D2F] text-2xl'>Our Menu</button>
             </div>
 
-            <div>
+            <div className='flex'>
                 <img src={bannerImage} alt=''/>
+                <img className='w-11 h-14' src={bannerImageElement} alt=''/>
             </div>
             
         </div>

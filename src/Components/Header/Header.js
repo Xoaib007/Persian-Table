884082ff5a2968dc37dc7689a9802b3e0971b4f1
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-04-14 02:33:52 +0200

    add

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import logo from '../../assets/logo.png'
 
 const Header = () => {
     return (
         <div>
-            hi
+            <img src={logo} alt=''/>
         </div>
     );
 };

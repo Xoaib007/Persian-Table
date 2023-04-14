ba1d40c619a65d3a19a36feb829c93cf83bbcec2
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-04-14 02:51:02 +0200

    add

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,13 @@ import logo from '../../assets/logo.png'
 const Header = () => {
     return (
         <div>
-            <img src={logo} alt=''/>
+            <div className='flex items-center'>
+                <img src={logo} alt='' />
+                <div className='flex flex-col items-start'>
+                <p className='text-white font-bold text-2xl'>Persian Table</p>
+                <p className='text-white font-bold text-2xl'>طاولة فارسية</p>
+                </div>
+            </div>
         </div>
     );
 };

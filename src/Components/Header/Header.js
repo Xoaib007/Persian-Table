cca5fa47b860f0809a6ff813bf958e53b072780f
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-04-14 16:01:20 +0200

    add

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from '../../assets/logo.png';
-import logo from '../../assets/logo.png'
+import book from '../../assets/bookNowBuuton.png'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
 
     const headerMenu = ['About', 'Menu', 'Venue', 'Events'];
 
     return (
-        <div className='h-36 flex  items-center'>
+        <div className='h-36 flex  justify-between px-10'>
             <div className='flex items-center'>
                 <img className='w-20 h-20 mr-2' src={logo} alt='' />
                 <div className='flex flex-col items-start'>
@@ -16,18 +17,17 @@ const Header = () => {
                 </div>
             </div>
 
-            <div className='ml-20 text-2xl text-white font-bold'>
-                <ul className='flex'>
-                    {
+            <div className='text-2xl text-white font-bold flex items-center'>
+                {
                         headerMenu.map(menu=>
-                            <li className='mx-5'>{menu}</li>
+                            <Link className='mx-5'>{menu}</Link>
                         )
                     }
-                </ul>
             </div>
 
-            <div>
-                <button className='w-64 h-16 bg-[#CC9D2F] text-2xl text-[#5B0017]'>Book Now</button>
+            <div className='flex items-center'>
+                <button className='w-40 h-16 bg-[#CC9D2F] text-2xl text-[#5B0017]'>Book Now</button>
+                <img src={book} alt=''/>
             </div>
         </div>
     );

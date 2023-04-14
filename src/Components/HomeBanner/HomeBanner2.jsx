75fcf03e923d00fcdfbcc7d68121085b001fea6d
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-04-15 00:35:30 +0200

    add

---
+++ src/Components/HomeBanner/HomeBanner2.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import HomeBanner2 from '../../assets/HomeBanner2.png';
+import Vanue1 from '../../assets/vanue1.png';
+import Vanue2 from '../../assets/vanue2.png';
+import Vanue3 from '../../assets/vanue3.png';
+
+const HomeBanner2 = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default HomeBanner2;
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../Components/Header/Header';
-import HomeBanner from '../Components/HomeBanner';
+import HomeBanner from '../Components/HomeBanner/HomeBanner';
+import HomeBanner2 from '../Components/HomeBanner/HomeBanner2';
 
 const HomePage = () => {
     return (
         <div className='bg-[#5B0017] min-h-screen'>
             <Header/>
             <HomeBanner/>
+            <HomeBanner2/>
         </div>
     );
 };

2fd7007b6e76c25351035c909cc8a0fbd2375f45
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-04-15 17:04:29 +0200

    add

--- src/Components/HomeBanner/HomeBanner3.js
+++ src/Components/HomeBanner/HomeBanner3.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import reviewer from '../../assets/review.png';
 import review from '../../assets/review1.png';
-import element from '../../assets/BannerElement2.png'
+import element from '../../assets/BannerElement2.png';
+import line from '../../assets/Line.png'
 
 const HomeBanner3 = () => {
     return (
-        <div className='flex mx-40'>
+        <div className='flex mx-20 mt-20 pb-60'>
             <div className='bg-[#CC9D2F]'>
-                <img src={element} alt='' />
+                <img className='' src={element} alt='' />
 
-                <div>
+                <div className='w-[363px] mr-16 ml-20'>
                     <div>
                         <p>“</p>
                         <p>You can't go wrong with Chicken Mandi, I had it twice. The chicken was cooked perfectly, juicy & soft (usually mandi chicken is a bit dry). I would defiantly recommend it.</p>
                     </div>
 
-                    <div>
-                        <div>
+                    <div className='flex justify-between mt-36 mb-2'>
+                        <div className='text-left'>
                             <p>Khalid Al Dawsry</p>
                             <p>Jeddah, Saudi</p>
                         </div>
 
                         <img src={reviewer} alt='' />
                     </div>
+
+                    <img src={line} alt=''/>
                 </div>
             </div>
 

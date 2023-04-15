69ca5fba07b683796c210ee15040d22e6e75f051
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-04-15 18:09:18 +0200

    add

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,13 +6,13 @@ const Footer = () => {
     const headerMenu = ['About', 'Menu', 'Venue', 'Events'];
 
     return (
-        <div className='h-80 bg-[#CC9D2F]'>
-            <div className='flex items-start bg-black w-fit px-6 py-2'>
+        <div className='h-80 bg-[#CC9D2F] pt-14'>
+            <div className='flex items-start bg-black w-fit px-6 py-2 mx-auto'>
                     <p className='text-white font-bold text-2xl mr-5'>Persian Table</p>
                     <p className='text-white font-bold text-2xl'>طاولة فارسية</p>
                 </div>
 
-            <div className='text-2xl text-white font-bold flex items-center'>
+            <div className='text-3xl text-white flex items-center justify-center'>
                 {
                         headerMenu.map((menu,i)=>
                             <Link to={`/${menu}`} className='mx-5' key={i}>{menu}</Link>

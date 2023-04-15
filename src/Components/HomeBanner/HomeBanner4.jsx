5646b1ef5079a07d92039fe8d2a944a3a985ff87
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-04-15 17:37:00 +0200

    add

--- src/Components/HomeBanner/HomeBanner4.jsx
+++ src/Components/HomeBanner/HomeBanner4.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import element from '../../assets/BannerElement.png'
+import element from '../../assets/BannerElement.svg'
 
 const HomeBanner4 = () => {
     return (
         <div className='bg text-white'>
             <div className='pt-40'>
-                <img className='mx-auto mt-40' src={element} alt='' />
+                <img className='mx-auto mt-20 w-80' src={element} alt='' />
                 <p className='text-5xl'>A unique menu that reflects the<br/> true essence of the Saudi cuisine</p>
                 <button>Our Menu</button>
             </div>

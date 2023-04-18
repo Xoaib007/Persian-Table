15abcaeee72d917d20fe42e1b134990d572fe6fc
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-04-18 20:08:58 +0200

    add

--- src/Components/HomeBanner/HomeBanner.jsx
+++ src/Components/HomeBanner/HomeBanner.jsx
@@ -4,16 +4,14 @@ import bannerImageElement from '../../assets/BannerElement.png'
 
 const HomeBanner = () => {
     return (
-        <div className='flex items-center justify-center banner'>
-            <div className='text-white flex flex-col items-start text-justify'>
-                <p className='header'>Taste the authentic <br/> Turkish cuisine</p>
-                <p className=''>Among the best Saudi chefs in the world,<br/>serving you something beyond flavor. </p>
-                <div className='w-80 border-4 border-indigo-500/100'>
+        <div className='flex items-center justify-center banner relative right-20'>
+            <div className='text-white flex flex-col items-start text-justify overlay'>
+                <p className='header'>Taste the authentic <br /> Turkish cuisine</p>
+                <p className=' text-2xl'>Among the best Saudi chefs in the world,<br />serving you something beyond flavor. </p>
                 <button className='button'>Our Menu</button>
-                </div>
             </div>
 
-            <div className='flex items-start'>
+            <div className='flex items-start bannerimage'>
                 <img src={bannerImage} alt='' />
                 <img className='element' src={bannerImageElement} alt='' />
             </div>

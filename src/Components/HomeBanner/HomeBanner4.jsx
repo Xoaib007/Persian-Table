6f04d2941f19bc16cfccfc8af3830c6733364031
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-04-18 16:10:02 +0200

    add

--- src/Components/HomeBanner/HomeBanner4.jsx
+++ src/Components/HomeBanner/HomeBanner4.jsx
@@ -4,7 +4,7 @@ import element from '../../assets/BannerElement.svg'
 
 const HomeBanner4 = () => {
     return (
-        <div className='bg text-white'>
+        <div className='bg text-white flex justify-center'>
             <div className='pt-40'>
                 <img className='mx-auto mt-20' src={element} alt='' />
                 <p className='text-5xl'>A unique menu that reflects the<br/> true essence of the Saudi cuisine</p>

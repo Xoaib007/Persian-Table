8a3cc8751c9a9f48e5ac632bb3e0e9a5d8f64320
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-04-20 16:07:09 +0200

    add

--- src/Components/AboutBanner/Milestones.js
+++ src/Components/AboutBanner/Milestones.js
@@ -2,30 +2,25 @@ import React from 'react';
 
 const Milestones = () => {
     return (
-        <div>
+        <div  className='text-left text-[#CC9D2F] bg-[#5B0017] flex'>
             <div>
-                <p>1998</p>
-                <p>Milestone I</p>
+                <p className='text-2xl text-white'>1998</p>
+                <p className='text-xl text-white'>Milestone I</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
             <div>
-                <p>2005</p>
-                <p>Milestone II</p>
+                <p className='text-2xl text-white'>2005</p>
+                <p className='text-xl text-white'>Milestone II</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
             <div>
-                <p>1998</p>
-                <p>Milestone I</p>
+                <p className='text-2xl text-white'>2012</p>
+                <p className='text-xl text-white'>Milestone III</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
             <div>
-                <p>1998</p>
-                <p>Milestone I</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-            <div>
-                <p>1998</p>
-                <p>Milestone I</p>
+                <p className='text-2xl text-white'>2021</p>
+                <p className='text-xl text-white'>Milestone IV</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
         </div>

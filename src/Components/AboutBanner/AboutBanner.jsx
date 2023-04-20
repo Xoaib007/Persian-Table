dcdd4439c18a34447041f0c7172b29e67973be9a
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-04-20 03:23:51 +0200

    add

--- src/Components/AboutBanner/AboutBanner.jsx
+++ src/Components/AboutBanner/AboutBanner.jsx
@@ -7,13 +7,13 @@ const AboutBanner = () => {
         <div className='flex justify-center aboutbanner'>
             <div className='flex flex-col title-box'>
                 <div className='title'>
-                    <p className='xll'>Our Story</p>
-                    <p className='mb-40'>We’re an absolute leader in the Saudi cuisine-themed restaurants. </p>
+                    <p className='xl'>Our Story</p>
+                    <p className='text-2xl'>We’re an absolute leader in the Saudi cuisine-themed restaurants. </p>
                 </div>
 
-                <div className='flex justify-center'>
+                <div className='flex justify-between'>
                     <img className='' src={img1} alt='' />
-                    <img src={img2} alt='' />
+                    <img className='mr-20' src={img2} alt='' />
                 </div>
             </div>
 
@@ -22,6 +22,7 @@ const AboutBanner = () => {
                 <p >Your one direction to high-quality Saudi feasts, since 1998 we served in Saudi Arabia.
                     <br/><br/>
                     Locals who are proud of the country and want to introduce it to visitors from around the world.
+                    <br/><br/>
                     We are perfectly positioned to provide our guests with an extraordinary health experience unlike any they’ve ever tasted. We can confidently claim that our menu is unlike anything else offered in the industry.
                     <br/><br/>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum finibus mauris nec tincidunt varius. Ut sagittis, lorem at bibendum ornare, mauris neque venenatis justo, ut gravida mi neque eu lorem. Vestibulum auctor leo sit amet vulputate mollis. Curabitur ac lorem felis. Morbi ullamcorper maximus lorem.</p>

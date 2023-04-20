fb7945a2ad865c0b51e651c1a4d5c315e0d2b943
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-04-20 15:52:41 +0200

    add

--- src/Components/AboutBanner/Milestones.js
+++ src/Components/AboutBanner/Milestones.js
@@ -3,7 +3,31 @@ import React from 'react';
 const Milestones = () => {
     return (
         <div>
-            
+            <div>
+                <p>1998</p>
+                <p>Milestone I</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+            <div>
+                <p>2005</p>
+                <p>Milestone II</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+            <div>
+                <p>1998</p>
+                <p>Milestone I</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+            <div>
+                <p>1998</p>
+                <p>Milestone I</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+            <div>
+                <p>1998</p>
+                <p>Milestone I</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
         </div>
     );
 };

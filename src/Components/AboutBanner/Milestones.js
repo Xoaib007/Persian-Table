c87fecf6fbab1bba9e21281712da74557447386c
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-04-20 16:26:48 +0200

    add

--- src/Components/AboutBanner/Milestones.js
+++ src/Components/AboutBanner/Milestones.js
@@ -4,22 +4,22 @@ const Milestones = () => {
     return (
         <div  className='text-left text-[#CC9D2F] bg-[#5B0017] flex mt-24 px-16 py-36 mx-36'>
             <div>
-                <p className='text-2xl text-white'>1998</p>
+                <p className='text-2xl text-white mb-5'>1998</p>
                 <p className='text-xl text-white'>Milestone I</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
             <div>
-                <p className='text-2xl text-white'>2005</p>
+                <p className='text-2xl text-white mb-5'>2005</p>
                 <p className='text-xl text-white'>Milestone II</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
             <div>
-                <p className='text-2xl text-white'>2012</p>
+                <p className='text-2xl text-white mb-5'>2012</p>
                 <p className='text-xl text-white'>Milestone III</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
             <div>
-                <p className='text-2xl text-white'>2021</p>
+                <p className='text-2xl text-white mb-5'>2021</p>
                 <p className='text-xl text-white'>Milestone IV</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>

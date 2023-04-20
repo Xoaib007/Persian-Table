fce99be2a49267d67d2959306ec8ca6c2dd325b3
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-04-21 01:08:43 +0200

    add

--- src/Components/AboutBanner/MapBannner.js
+++ src/Components/AboutBanner/MapBannner.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Map from './Map';
 
 const MapBannner = () => {
     return (
         <div className='bg-[#243054]'>
-
-            
+            <div>
+                <p>Open Hours</p>
+                <p>Sunday to Saturday<br/>09:00 AM to 11:00 PM</p>
+                <p>Friday<br/>02:00 PM to 1:00 AM</p>
+                <p>Location</p>
+                <p>Street - 127, Jeddah,Saudi Arabia54654</p>
+                <p>Contact us</p>
+                <p>+123456789<br/>service@maizresturant.com</p>
+            </div>
+            <Map />
         </div>
     );
 };
--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import AboutBanner from '../Components/AboutBanner/AboutBanner';
 import Milestones from '../Components/AboutBanner/Milestones';
-import Map from '../Components/AboutBanner/Map';
+import MapBannner from '../Components/AboutBanner/MapBannner';
 
 const AboutPage = () => {
     return (
         <div className='min-h-screen bg-[#CC9D2F]'>
             <AboutBanner/>
             <Milestones/>
-            <Map/>
+            <MapBannner/>
         </div>
     );
 };

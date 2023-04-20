092b042d303772103313bee37738ff42ae8ec93f
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-04-20 16:33:09 +0200

    add

--- src/Components/AboutBanner/Milestones.js
+++ src/Components/AboutBanner/Milestones.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Milestones = () => {
     return (
-        <div  className='text-left text-[#CC9D2F] bg-[#5B0017] flex mt-24 px-16 py-36 mx-36'>
+        <div  className='text-left text-[#CC9D2F] bg-[#5B0017] flex mt-24 pb-24 px-16 py-36 mx-36'>
             <div>
                 <p className='text-2xl text-white mb-5'>1998</p>
                 <p className='text-xl text-white'>Milestone I</p>
--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import AboutBanner from '../Components/AboutBanner/AboutBanner';
 import Milestones from '../Components/AboutBanner/Milestones';
+import Map from '../Components/AboutBanner/Map';
 
 const AboutPage = () => {
     return (
         <div className='min-h-screen bg-[#CC9D2F]'>
             <AboutBanner/>
             <Milestones/>
+            <Map/>
         </div>
     );
 };

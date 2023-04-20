e32eaff05d54a9d31c39f57be98f7f67d2818b19
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-04-20 15:35:13 +0200

    add

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AboutBanner from '../Components/AboutBanner/AboutBanner';
+import Milestones from '../Components/AboutBanner/Milestones';
 
 const AboutPage = () => {
     return (
         <div className='min-h-screen bg-[#CC9D2F]'>
             <AboutBanner/>
+            <Milestones/>
         </div>
     );
 };

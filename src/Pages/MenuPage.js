ee41bcf3452a3967a46cdfa3a76257ea850f75eb
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-04-21 11:17:11 +0200

    add

--- src/Pages/MenuPage.js
+++ src/Pages/MenuPage.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const MenuPage = () => {
     return (
-        <div className='min-h-screen bg-[#CC9D2F]'>
-            
+        <div className='min-h-screen bg-[#5B0017]'>
+            <div className='flex'>
+                <p className='xl'>Discover Our Menu</p>
+                <p>There is a wide selection of choices served by top Saudi chefs, prepared using best quality local ingredients.</p>
+            </div>
         </div>
     );
 };

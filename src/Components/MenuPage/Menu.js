b71a6cadaa8e8790736f2d7155eb9fcea0386299
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-04-21 13:31:56 +0200

    add

---
+++ src/Components/MenuPage/Menu.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Menu = () => {
+    return (
+        <div className='bg-[#5B0017] min-h-screen'>
+            
+        </div>
+    );
+};
+
+export default Menu;

1381b42ba90a56ac2fdce731d99944253212ec60
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-04-21 14:36:22 +0200

    add

--- src/Components/MenuPage/Menu.js
+++ src/Components/MenuPage/Menu.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Document, Page } from 'react-pdf';
+import pdf from '../../assets/ALA-TURKISH-MENU.pdf'
 
 const Menu = () => {
+
+    const [numPages, setNumPages] = useState(null);
+
+    function onDocumentLoadSuccess({ numPages }) {
+        setNumPages(numPages);
+    }
+
     return (
         <div className='bg-[#5B0017] min-h-screen'>
-            
+            <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
+                {
+                    Array.from( new Array(numPages), (el, index) => (
+                        <Page
+                            key={`page_${index + 1}`}
+                            pageNumber={index + 1}
+                        />
+                    )
+                )}
+            </Document>
+
         </div>
     );
 };

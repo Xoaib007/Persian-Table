9297e779bc24e3a12e86e2bd7da60e8aba8cf310
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-04-27 00:58:42 +0200

    add

--- src/Components/MenuPage/Menu.jsx
+++ src/Components/MenuPage/Menu.jsx
@@ -6,37 +6,37 @@ import './Menu.css'
 const Menu = () => {
 
     const [numPages, setNumPages] = useState(null);
-	const [pageNumber, setPageNumber] = useState(1);
+    const [pageNumber, setPageNumber] = useState(1);
 
-	const onDocumentLoadSuccess = ({ numPages }) => {
-		setNumPages(numPages);
-	};
+    const onDocumentLoadSuccess = ({ numPages }) => {
+        setNumPages(numPages);
+    };
 
-	const goToPrevPage = () =>
-		setPageNumber(pageNumber - 1 <= 1 ? 1 : pageNumber - 1);
+    const goToPrevPage = () =>
+        setPageNumber(pageNumber - 1 <= 1 ? 1 : pageNumber - 1);
 
-	const goToNextPage = () =>
-		setPageNumber(
-			pageNumber + 1 >= numPages ? numPages : pageNumber + 1,
-		);
+    const goToNextPage = () =>
+        setPageNumber(
+            pageNumber + 1 >= numPages ? numPages : pageNumber + 1,
+        );
 
     return (
         <div className='bg-[#5B0017] min-h-screen'>
             <nav>
-        <button onClick={goToPrevPage} className="previous">
-          Prev
-        </button>
-        <button onClick={goToNextPage} className="next">
-          Next
-        </button>
-        <p>
-          Page {pageNumber} of {numPages}
-        </p>
-      </nav>
+                <button onClick={goToPrevPage} className="previous">
+                    Prev
+                </button>
+                <button onClick={goToNextPage} className="next">
+                    Next
+                </button>
+                <p>
+                    Page {pageNumber} of {numPages}
+                </p>
+            </nav>
 
-      <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
-      </Document>
+            <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
+                <Page pageNumber={pageNumber} />
+            </Document>
         </div>
     );
 };
